0f34295fc00589cd071ae5afdc3a6842c85eacef
Catie <130665030+Cat-Ling@users.noreply.github.com>
2025-03-08 10:51:54 +0000

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -2,7 +2,7 @@
 set -e
 
 apk add --no-cache curl tar
-VERSION=$(curl -sSL https://github.com/5rahim/seanime/releases/latest/download/latest.json | grep -oP '(?<="version": ")[^"]*')
+VERSION=$(curl -sSL https://github.com/5rahim/seanime/releases/latest/download/latest.json | grep -oE '"version": "[^"]*' | cut -d'"' -f4)
 curl -sSLO https://github.com/5rahim/seanime/releases/latest/download/seanime-${VERSION}_Linux_x86_64.tar.gz
 tar -xzf seanime-${VERSION}_Linux_x86_64.tar.gz
 chmod +x seanime
